a110d402eec3ce184c4dfdee1885e1ec3d620549
totaltaxamount <shieldscoen@gmail.com>
2024-10-08 14:35:15 -0600

    update: Config manager stuff

--- src/main/java/frc/robot/utils/ConfigManager.java
+++ src/main/java/frc/robot/utils/ConfigManager.java
@@ -1,5 +1,6 @@
 package frc.robot.utils;
 
+import edu.wpi.first.networktables.NetworkTableEvent;
 import edu.wpi.first.wpilibj.Filesystem;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -7,22 +8,21 @@ import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.EnumSet;
 import java.util.Objects;
-import java.util.Scanner;
 
 public class ConfigManager {
     private static ConfigManager INSTANCE;
 
     private final File configFile = Path.of(Filesystem.getDeployDirectory().toPath().toString(), "tuning.json").toFile();
 
-    private final HashMap<String, Double> data = new HashMap<>();
-
     private JSONObject json;
 
+    private NetworkTableUtils NTTune = new NetworkTableUtils("Tune");
+
     /**
-     * Get the instance of the config utils
-     * @return Instance of config utils
+     * Get the instance of the config manager
+     * @return Instance of config manager
      */
     public static ConfigManager getInstance() {
         if (INSTANCE == null) {
@@ -34,8 +34,8 @@ public class ConfigManager {
 
     /**
      * Util class to allow for good network table tuning
-     * TODO: Integrate better with network tables with (Maybe with {@link edu.wpi.first.networktables.NetworkTableListener})
      */
+    // TODO: Add support for vales besides doubles
     public ConfigManager() {
         try {
             if (configFile.createNewFile() || configFile.length() == 0) {
@@ -48,6 +48,20 @@ public class ConfigManager {
         }
 
         this.json = parseConfig();
+        this.initListener();
+    }
+
+
+    /**
+     * Add a listener to network tables for a change in one of the tuning values
+     */
+    @SuppressWarnings("unchecked")
+    private void initListener() {
+        NTTune.getTable().addListener((EnumSet.of(NetworkTableEvent.Kind.kValueAll)), (table, key1, event) -> {
+            this.json.put(key1, table.getValue(key1).getValue());
+            System.out.println("[DEBUG] Updated [" + key1 + "] to " + table.getEntry(key1).getDouble(-1));
+            this.saveConfig();
+        });
     }
 
     /**
@@ -81,29 +95,87 @@ public class ConfigManager {
     }
 
     /**
-     * Get a double from the config
-     * @param key The key in the json
+     * Get a value from the config
+     * @param key          The key in the json
      * @param defaultValue A default value in case we fail to get the key
      * @return The value from the key
      */
-    public double getDouble(String key, double defaultValue) {
+    @SuppressWarnings("unchecked")
+    public <T> T get(String key, Class<T> type, T defaultValue) {
+        if (!NTTune.getTable().getEntry(key).exists()) {
+            System.out.println("[WARN] " + key + " does not exist in network tables, creating a setting to " + defaultValue);
+
+        }
+        if (type.equals(Double.class) || type.equals(Integer.class)) {
+            return (T) getDouble(key, (double)defaultValue);
+        } else if (type.equals(String.class)) {
+            return (T) getString(key, (String) defaultValue);
+        } else if (type.equals(Boolean.class)) {
+            return (T) getBoolean(key, (boolean) defaultValue);
+        }
+
+        return defaultValue;
+    }
+
+    /**
+     * Get a double from the config
+     * @param key The key in the json
+     * @param defaultValue A default value
+     * @return A double (as an {@link Object})
+     */
+    private Object getDouble(String key, double defaultValue) {
         double res = defaultValue;
         try {
             res = (double) this.json.get(key);
-        } catch (Exception e) {
-            System.out.println("[WARN] Failed to get " + key + ": " + e);
+        } catch (ClassCastException e) {
+            System.out.println("[WARN] Failed to get " + key + " as a double");
         }
 
         return res;
-     }
+    }
 
     /**
-     * Set a double value
+     * Get a Boolean from the config
+     * @param key The key in the json
+     * @param defaultValue A default value
+     * @return A boolean (as an {@link Object})
+     */
+    private Object getBoolean(String key, boolean defaultValue) {
+        boolean res = defaultValue;
+        try {
+            res = (boolean) this.json.get(key);
+        } catch (ClassCastException e) {
+            System.out.println("[WARN] Failed to get " + key + " as a boolean");
+        }
+
+        return res;
+    }
+
+    /**
+     * Get a string from the config
+     * @param key The key in the json
+     * @param defaultValue A default value
+     * @return A string (as an {@link Object})
+     */
+    private Object getString(String key, String defaultValue) {
+        String res = defaultValue;
+        try {
+            res = (String) this.json.get(key);
+        } catch (ClassCastException e) {
+            System.out.println("[WARN] Failed to get " + key + " as a string");
+        }
+
+        return res;
+    }
+
+
+    /**
+     * Set a value
      * @param key The key for the json file
      * @param value The value to set
      */
      @SuppressWarnings("unchecked")
-     public void setDouble(String key, double value) {
+     public <T> void set(String key, T value) {
         this.json.put(key, value);
         this.saveConfig();
      }
